29d7f3ea79f2920ad272cd00e97da25edd3dbbb9
amal <muslim.amal@yandex.ru>
2018-01-20 23:48:25 +0300

    rea

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import constants
 bot = telebot.TeleBot(constants.token)
 upd = bot.get_updates()
 last_upd = upd[-1]
-file = open("city.txt",'r+', errors='ignore')
+file = open("city.txt",'br', errors='ignore')
 cities = ""
 
 
@@ -34,7 +34,7 @@ def handle_text(message):
             bot.send_message(message.chat.id, "Поехали, посмотрим на что ты способен  ")
             send.clear()
             send.append("!")
-            file = open("city.txt",'r+', errors='ignore')
+            file = open("city.txt",'br', errors='ignore')
             cities = file.read()
             all_cities.clear()
         elif(cities.find(message.text)!=-1 and out(send,message) and message.text not in all_cities):
