894b737ce981bf0927024697aa0ffe935d1267aa
amal <muslim.amal@yandex.ru>
2018-01-20 22:54:35 +0300

    rea

--- main.py
+++ main.py
@@ -60,8 +60,6 @@ def handle_text(message):
             bot.send_message(message.chat.id, "Тебе со мной не тягаться, ты проиграл")
     except(IndexError):
         bot.send_message("Я проиграл")
-    except:
-        bot.send_message(message.chat.id, "Я проиграл, больше не знаю городов на эту букву")
 
 
 bot.polling(none_stop=True, interval=0)
