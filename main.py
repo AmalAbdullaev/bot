97bcf62c3d76476e1d09ec1b9d447717c21e9076
amal <muslim.amal@yandex.ru>
2018-01-20 22:44:31 +0300

    Сделал совсем по другому,теперь читает города с файла, изменил полностью логику

--- main.py
+++ main.py
@@ -6,44 +6,60 @@ import copy
 bot = telebot.TeleBot(constants.token)
 upd = bot.get_updates()
 last_upd = upd[-1]
-cities = copy.deepcopy(goroda.city)
-results = []
-letter = "!"
+file = open("city.txt",'r+')
+cities = ""
+print(cities)
+
+send = ["!"]
+all_cities = []
+
+def out(send,message):
+    el = send.pop()
+    if(el == "!" or el[len(el)-1].upper()==message.text[0]):
+        return True
+    elif(el=="!" or el[len(el)-1].upper()=="Ь" or el[len(el)-1].upper()=="Ъ" or el[len(el)-1].upper()=="Ы" ):
+        if(el[len(el)-2].upper()==message.text[0]):
+            return True
+        else:
+            return False
+    else:
+        return False
+
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
+    global start
+    global cities
+    global file
     try:
-        global cities, letter
-        client_city = message.text
-
-        if(message.text == "/start"):
-            bot.send_message(message.chat.id, "Отправь мне город, города не должны начинаться и заканчиваться Ь,Й,Ы,Ъ, если я  назвал такой город"
-                                              " значит я продул, да да и такое бывает "
-                                              ""
-                                              " В любом случае жми /start и будем играть ")
-            results.clear()
-            letter = "!"
-        elif((letter==client_city[0] or letter== "!") and client_city in cities.get(client_city[0].lower()) and client_city not in results ):
-            my_city = cities.get(client_city[len(client_city)-1].lower()).pop()
-            if(my_city[0]!=client_city[len(client_city)-1]):
-                my_city = cities.get(client_city[len(client_city) - 1].lower()).pop()
-            letter = my_city[len(my_city) - 1].upper()
-            if(letter=="ь" or letter == "ы" or letter=="ъ" or letter=="й"):
-                letter = my_city[len(my_city) - 2].upper()
-            bot.send_message(message.chat.id, my_city)
-            results.append(message.text)
-            results.append(my_city)
+        if (message.text == "/start"):
+            bot.send_message(message.chat.id, "Поехали, посмотрим на что ты способен  ")
+            send.clear()
+            send.append("!")
+            file = open("city.txt",'r+')
+            cities = file.read()
+            all_cities.clear()
+        elif(cities.find(message.text)!=-1 and out(send,message) and message.text not in all_cities):
+            cities = cities.replace(message.text, '')
+            start = cities.find(message.text[len(message.text)-1].upper()) #ищем город на последнюю букву города которую назвал клиент
+            if (start == -1):
+                start = cities.find(message.text[len(message.text) - 2].upper())
+            end  =  cities.find("\n",start)
+            answer_city =  cities[start:end]
+            bot.send_message(message.chat.id, answer_city)
+            send.append(answer_city)
+            all_cities.append(answer_city)
+            all_cities.append(message.text)
+            cities = cities.replace(answer_city,'')
+        elif(message.text[0] == "."):
+            city = message.text[1:]
+            file.write("\n" + city)
+            bot.send_message(message.chat.id, "Запомнил")
 
         else:
-            bot.send_message(message.chat.id,"Тебе со мной не тягаться, ты проиграл, давай сначала, пиши /start и начнем")
-            results.clear()
-            letter = "!"
+            bot.send_message(message.chat.id, "Тебе со мной не тягаться, ты проиграл")
+    except:
+        bot.send_message(message.chat.id, "Я проиграл, больше не знаю городов на эту букву")
 
-    except(IndexError):
-        bot.send_message(message.chat.id, "Я проиграл, больше не знаю городов на эту букву, давай еще, пиши /start и начнем")
-        cities = copy.deepcopy(goroda.city)
-    except(AttributeError):
-        bot.send_message(message.chat.id,
-                         "Котики, пожалуйста, не называйте города которые начинаются и заканчиватся на Ь,Й,Ы,Ъ")
 
 bot.polling(none_stop=True, interval=0)
